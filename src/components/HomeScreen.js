474d22b844bf9bf9347294973aba9a19006b4952
Jeet Hathiwala <jhathiwala@gmail.com>
2017-07-30 17:41:40 +0530

    Created the basic menu. Hardcoded. Optimizations to be performed later.

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,19 +1,124 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';	
+
+/*
+class NavigationButton extends React.Component {
+	constructor(props) {
+		super(props);
+		this.text = this.props.text;
+		this.imgSrc = this.props.imgSrc;
+	}
+	render() {
+		console.log("Image: " + this.imgSrc);
+		let image = require(this.imgSrc);
+		return (
+			<View>
+				<Image
+					source={image}
+					style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+					<Text>{this.text}</Text>
+				</Image>
+			</View>
+		);
+	}
+}
+*/
 
 export default class HomeScreen extends React.Component {
 	static navigationOptions = {
 		title: 'Home'
 	};
 
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return (
-			<View>
-				<Text onPress={() => this.props.navigation.navigate('WIP')}>
-					Open up App.js to start working on your app! This is actually from the components directory. Woo hoo!
-				</Text>
+			<View style={styles.rootView}>
+				<Text>Started</Text>
+				<View style={styles.buttonContainer}>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 1</Text>
+						</Image>
+					</View>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 2</Text>
+						</Image>
+					</View>
+				</View>
+				<View style={styles.buttonContainer}>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 3</Text>
+						</Image>
+					</View>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 4</Text>
+						</Image>
+					</View>
+				</View>
+				<View style={styles.buttonContainer}>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 5</Text>
+						</Image>
+					</View>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 6</Text>
+						</Image>
+					</View>
+				</View>
+				<View style={styles.buttonContainer}>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 7</Text>
+						</Image>
+					</View>
+					<View>
+						<Image
+							source={require('../img/circleplaceholder.png')}
+							style={{height: 100, width: 100, alignItems: 'center', justifyContent: 'center'}}>
+							<Text>Button 8</Text>
+						</Image>
+					</View>
+				</View>
+				<Text>Ended</Text>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	rootView: {
+		flexDirection: 'column',
+		flex: 1,
+		width: Dimensions.get('window').width
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		flex: 1,
+		width: Dimensions.get('window').width,
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	}
+});
